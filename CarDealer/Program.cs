8e911bf45ba3bfa48fa7c77c72c6d2f266c8f201
Garan Weber <garanw@hotmail.com>
2016-10-09 14:04:21 -0500

    Added Comments

--- CarDealer/Program.cs
+++ CarDealer/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+// Advanced C# Class Module 7
+// Car Dealership Program 
+// Showcases the use of constructors
+// including overloaded
+// also shows use of static vs non-static methods
+// Written by Garan Weber
+// 10/09/2016
 
 namespace CarDealer
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            // Calls static Method
+            // shows that an instance of the class is not required to call
+            // placed here to illustrate that this can be done before any
+            // instance is created
+            Car.CarHonk();
+            // Instantiates 2 instances of the Car class
+            // the first uses one constructor and the second
+            // uses another
             Car myCar = new Car("Chevy");
             myCar.Model = "Malibu LT";
             myCar.Year = 2010;
@@ -24,35 +38,45 @@ namespace CarDealer
             Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
             Console.WriteLine("{0} {1} {2} {3}", myOtherCar.Make, myOtherCar.Model, myOtherCar.Year, myOtherCar.Color);
 
-
-            Car.CarHonk();
+            // Calls non-static method
+            // can not be called without and instance to call it from
             myCar.CarGasType();
 
+
+            // so you have time to read
             Console.ReadKey();
         }
     }
     class Car
     {
+
+        // Default Setters and Getters for each property
         public string Make { get; set; }
         public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
 
+
+        // Default constructor Constructor with no values
         public Car()
         {
 
         }
 
+        // Overloaded Constructor to allow passing Make on instantiation
         public Car(string make)
         {
             Make = make;
         }
 
+        // Static Method can be called without instantiation
         public static void CarHonk()
         {
             Console.WriteLine("The car says Honk.");
         }
 
+        // non-static method, can not be called without a valid instance
+        // of the Car Class
         public void CarGasType()
         {
             Console.WriteLine("This car uses Premium gas.");
