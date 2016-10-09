05b303251a4c70e1343dc23397981f4d64343b41
Garan Weber <garanw@hotmail.com>
2016-10-09 13:52:41 -0500

    Initial Car Dealer Program for Advanced C# Class.

---
+++ CarDealer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Chevy");
+            myCar.Model = "Malibu LT";
+            myCar.Year = 2010;
+            myCar.Color = "Maroon";
+
+            Car myOtherCar = new Car();
+            myOtherCar.Make = "Nissan";
+            myOtherCar.Model = "Versa Note";
+            myOtherCar.Year = 2014;
+            myOtherCar.Color = "Silver";
+
+            Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+            Console.WriteLine("{0} {1} {2} {3}", myOtherCar.Make, myOtherCar.Model, myOtherCar.Year, myOtherCar.Color);
+
+
+            Car.CarHonk();
+            myCar.CarGasType();
+
+            Console.ReadKey();
+        }
+    }
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public Car()
+        {
+
+        }
+
+        public Car(string make)
+        {
+            Make = make;
+        }
+
+        public static void CarHonk()
+        {
+            Console.WriteLine("The car says Honk.");
+        }
+
+        public void CarGasType()
+        {
+            Console.WriteLine("This car uses Premium gas.");
+
+        }
+    }
+}
